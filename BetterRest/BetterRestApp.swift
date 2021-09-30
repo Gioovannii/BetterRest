6a84c34b461decba374a60b65e7640e84c3a52d5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-30 11:39:17 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Giovanni Gaffé on 2021/9/30.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
