92637e76e1ecd9dab400ae34472790ce054a9434
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-01 11:16:01 +0800

    Add some property for alert

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         
