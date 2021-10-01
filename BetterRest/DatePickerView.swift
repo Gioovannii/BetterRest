8d94b290a8901df8200a810647994de9009d9b27
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-01 10:58:47 +0800

    Create new file with dates

---
+++ BetterRest/DatePickerView.swift
@@ -0,0 +1,36 @@
+//
+//  DatePickerView.swift
+//  BetterRest
+//
+//  Created by Giovanni Gaffé on 2021/9/30.
+//
+
+import SwiftUI
+
+struct DatePickerView: View {
+    @State private var wakeUp = Date()
+    var body: some View {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        let dateString = formatter.string(from: Date())
+        
+        return DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+            
+    }
+}
+
+struct DatePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerView()
+    }
+}
+
+
+//        Set time
+//        let now = Date()
+//        let tomorrow = Date().addingTimeInterval(86400)
+//        let range = now ... tomorrow
+        
+//          Set date in the future
+//          in: Date()...
